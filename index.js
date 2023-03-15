569b615a1cc734904d75770e25893d36aed9aac6
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2023-03-15 22:02:53 +0700

    test: add integration test

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
-const express = require('express')
 
+const app = require('./app')
 const port = process.env.PORT
-const app = express()
-const authRouter = require('./domains/auth/auth.routes')
-
-app.use(express.json())
-
-app.use('/v1', authRouter)
-
-app.use((err, req, res, next) => {
-    console.log(err)
-    const statusCode = err.code || 500
-    const errorData = err || {'message': 'Internal Server Error'}
-
-    return res.status(statusCode).json(errorData)
-})
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
